6fc61379e56aeebb4d344fd5e3766f927ccf61e9
KeSuDr <eurkung@gmail.com>
2024-03-26 16:20:50 +0700

    vercel

--- server.js
+++ server.js
@@ -64,7 +64,9 @@ app.use('/api/v1/appointments',appointments);
 
 const PORT=process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in '
+    , process.env.NODE_ENV
+    , ' mode on port ', PORT));
 
 //Handle unhandled promise rejection
 process.on('unhandledRejection',(err,promise)=>{
@@ -88,7 +90,7 @@ const swaggerOptions={
         },
         servers: [
             {
-                url: 'http://localhost:5000/api/v1'
+                url: process.env.HOST + ":" + PORT + '/api/v1'
             }
         ],
 
